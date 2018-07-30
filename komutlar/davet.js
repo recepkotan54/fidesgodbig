9f73b8082008897206c62af509b6ebc27cbe9464
recepkotan54 <41400042+recepkotan54@users.noreply.github.com>
2018-07-30 15:27:57 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -13,7 +13,7 @@ exports.run = (client, message) => {
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('**__Buyrun Bizim Sunucuya__** :)  https://discord.gg/Ee7hBtV **Yapımcılar**: Recep | Fides#9190');
+    .setDescription('**__Buyrun Bizim Sunucuya__** :)  https://discord.gg/Ee7hBtV **Yapımcım**: $β ♕ Simba Owner#2913');
     return message.author.sendEmbed(pingozel)
 };
 
